d86f0fe7fee8566b3ab81c7a11f50d1910080c03
SamusDeCereza <20233l001130@utcv.edu.mx>
2024-10-04 03:31:43 -0600

    feat: Se añadio la clase prenda para uso en el proyecto

---
+++ prenda.java
@@ -0,0 +1,49 @@
+public class prenda {
+    private String nombre;
+    private String diseniador;
+    private int anioColeccion;
+    private float precio;
+
+
+    public prenda(String nombre, String diseniador, int anioColeccion, float precio){
+        this.nombre = nombre;
+        this.diseniador = diseniador;
+        this.anioColeccion = anioColeccion;
+        this.precio = precio;
+
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setDiseniador(String diseniador) {
+        this.diseniador = diseniador;
+    }
+
+    public String getDiseniador() {
+        return diseniador;
+    }
+
+    public int getAnioColeccion() {
+        return anioColeccion;
+    }
+
+    public void setAnioColeccion(int anioColeccion) {
+        this.anioColeccion = anioColeccion;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+}
+
