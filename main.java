410bc9271b1d0cf8ba097680a25809c1ac0220e8
SamusDeCereza <20233l001130@utcv.edu.mx>
2024-10-04 03:52:34 -0600

    feat: Añadido un menú contextual para navegar entre las opciones

--- main.java
+++ main.java
@@ -5,6 +5,39 @@ public class main {
 
     public static void main(String[] args) {
         
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Bienvenido a Boutique Elegance");
+
+        String respuesta = "si";
+        int opcion = 0;
+
+        while (respuesta.equals("si")) {
+            opcion = mostrarMenu(entrada);
+
+
+            System.out.println("¿Desea realizar alguna otra operación? (si/no)");
+            respuesta = entrada.nextLine().toLowerCase();
+            respuesta = entrada.nextLine().toLowerCase();
+            System.out.println(respuesta);
+        }
+    }
+
+    public static int mostrarMenu(Scanner entrada){
+
+        int respuesta = 0;
+
+        System.out.println("Seleccione una opción");
+        System.out.println("1 - Buscar una prenda");
+        System.out.println("2 - Registrar una prenda");
+        System.out.println("3 - Ordenar prendas (año de colección)");
+        System.out.println("4 - Verificar el valor total del inventario");
+
+        respuesta = entrada.nextInt();
+
+
+        return respuesta;
+
     }
 
 }
